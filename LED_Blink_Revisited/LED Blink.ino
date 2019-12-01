21fffa02e68030eff1abdaf1d8b60209c09ed9cb
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2019-12-01 17:43:19 -0500

    Commented code

--- LED_Blink_Revisited/LED Blink.ino
+++ LED_Blink_Revisited/LED Blink.ino
@@ -1,12 +1,12 @@
 
 
-int led = 11;
-int brightness = 0;
-int fadeAmount = 10;
+int led = 11; //LED pin
+int brightness = 0; //sets original brightness
+int fadeAmount = 10; //variable for change in brightness
 
 void setup() {
  
-  pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT); //Sets LED pin as OUTPUT
 }
 
 void loop() {
@@ -14,7 +14,7 @@ void loop() {
   if(brightness<200){
 
   analogWrite(led, brightness);
-  brightness = brightness + fadeAmount; 
+  brightness = brightness + fadeAmount;  //When brightness is less than 200 it increases in increments of 10
 
   }
  
@@ -22,8 +22,8 @@ void loop() {
   if(brightness=200){
 
   analogWrite(led, brightness);
-  brightness = 0; 
-  delay(1000);
+  brightness = 0;  //Once brightness reaches 200 it reverts to 0 and decreases again
+  delay(1000); //The delay is nessecary so fade is slow enough to be noticeable
   
   }
 
