0fb6379797efe7dd5ae8085d5817ad4b42e38d78
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2019-12-01 17:56:28 -0500

    Commented code

--- Photointerrupter_LED_1/photointerrupterled1.ino
+++ Photointerrupter_LED_1/photointerrupterled1.ino
@@ -2,16 +2,16 @@
 int photoPin = 2;
 int ledPin = 8;
 int photoState = 0;
-int interrupt = 0;
+int interrupt = 0; //The interrupt variable counts how many times to photointerrupter has sensed something 
 
-int on(){
+int on(){  //LED functions are set as integers instead of in void loop()
 digitalWrite(ledPin, HIGH);
 delay(100);
-interrupt = interrupt +1;
+interrupt = interrupt +1; //The varible interrupt increases everytime the LED blinks
 }
 
 int off() {
-digitalWrite(ledPin, LOW);
+digitalWrite(ledPin, LOW);//When the photointerrupter is not activated, the LED is off
 delay(100);
 }
 
@@ -21,14 +21,14 @@ void setup(){
 Serial.begin(9600);
 pinMode(photoPin, INPUT_PULLUP);
 pinMode (ledPin, OUTPUT);
-attachInterrupt(digitalPinToInterrupt(3), off, FALLING);
-attachInterrupt(digitalPinToInterrupt(2), on, RISING);
+attachInterrupt(digitalPinToInterrupt(3), off, FALLING); //When the photointerrupter senses falling movement, int off() is run
+attachInterrupt(digitalPinToInterrupt(2), on, RISING); //When the photointerrupter senses rising movement, int on() is run
 
 }
 
 void loop() {
 if(on){
-Serial.println(interrupt);
+Serial.println(interrupt); //Printing the variable interrupt allows the Serial Monitor to count LED blinks/photointerrupter activations
 delay(100);
 }
 }
