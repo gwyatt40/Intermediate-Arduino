740b5b09958525e0f1bad522ffe7dbcebc5587a1
Georgia E Wyatt <gwyatt40@ccs.local>
2019-12-04 11:38:04 -0500

    deleted old Pint2 code and renamed Pint1 code file

---
+++ Photointerrupter_LED_1/PhotointerrupterLED_1.ino
@@ -0,0 +1,35 @@
+
+int photoPin = 2;
+int ledPin = 8;
+int photoState = 0; //Sets photoState variable 
+
+
+
+void setup(){
+Serial.begin(9600);
+pinMode(photoPin, INPUT_PULLUP);
+pinMode (ledPin, OUTPUT);
+
+
+}
+
+void loop() {
+
+
+photoState = digitalRead(photoPin); //photoState works the same as a buttonState variable, except reading photointerrupter 
+ 
+  	  if (photoState == HIGH) { //When photointerrupter activated, LED on
+  	  	digitalWrite(ledPin, HIGH);
+  	  	Serial.println("On!"); //serial monitor to check in case of issue w/ wiring
+  	  }
+
+
+  	  if(photoState == LOW) { //When photointerrupter clear, LED off
+  	   digitalWrite(ledPin, HIGH);
+  	   Serial.println("Off!"); 
+
+  	  }
+
+
+
+}
